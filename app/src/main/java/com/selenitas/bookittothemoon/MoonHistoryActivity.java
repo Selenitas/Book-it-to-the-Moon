43003316be059877635c1cd906aff5183d8abd18
CristhianMotoche <cristhian.motoche@gmail.com>
2016-05-03 15:59:07 -0500

    Add moon history

---
+++ app/src/main/java/com/selenitas/bookittothemoon/MoonHistoryActivity.java
@@ -0,0 +1,13 @@
+package com.selenitas.bookittothemoon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoonHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moon_history);
+    }
+}
